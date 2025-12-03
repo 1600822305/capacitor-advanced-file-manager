95c0e18f106cb05466aef93eeaa21e7149e246b2
q1600822305 <q1600822305@noreply.gitcode.com>
2025-12-04 00:44:49 +0800

    chore: cleanup temp files and bump to 0.4.0

--- package/src/modules/batch/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-import type { BatchOperationsPlugin } from './definitions';
-
-const BatchOperations = registerPlugin<BatchOperationsPlugin>('BatchOperations', {
-  web: () => import('./web').then(m => new m.BatchOperationsWeb()),
-});
-
-export * from './definitions';
-export { BatchOperations };
--- package/src/modules/search/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-import type { FileSearchPlugin } from './definitions';
-
-const FileSearch = registerPlugin<FileSearchPlugin>('FileSearch', {
-  web: () => import('./web').then(m => new m.FileSearchWeb()),
-});
-
-export * from './definitions';
-export { FileSearch };
